1f10832d145782199da47725d76df9bbeb1af9a3
calebredd <caleb.redd@yahoo.com>
2019-08-01 13:48:00 -0700

    Imported Router components/modules and enabled navbar styling. Converted to sass styling. Added colors to buttons. Removed text decoration on hover. Displayed path components below navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import './App.css';
-import { Home, About, Contact, Navigation } from './components';
+import React from "react";
+import { Home, About, Contact, Navigation } from "./components";
+import { Route } from "react-router-dom";
+
+import "./App.scss";
 
 const App = () => (
-  <div>
+  <div className="App">
     <Navigation />
+    <div className="main">
+      <Route exact path="/" component={Home} />
+      <Route path="/contact" component={Contact} />
+      <Route path="/about" component={About} />
+    </div>
   </div>
 );
 
